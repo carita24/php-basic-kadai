38af622dcfb834af9d5cff290ccab58a5ef2da58
carita24 <153448483+carita24@users.noreply.github.com>
2023-12-13 03:58:16 +0900

    kadai_016ファイルをアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        class Food{
+            private $name;
+            private $price;
+
+            public function show_price(){
+                echo $this->price  .'<br>';
+            }    
+
+            public function __construct(string $name,int $price){
+                $this->name =$name;
+                $this->price =$price;
+            }
+                
+        }
+        $Food = new Food('potato',250);
+
+        class Animal{
+            private $name;
+            private $height;
+            private $weight;
+            
+            public function show_height(){
+                echo $this->height .'<br>';
+            }
+
+            public function __construct(string $name,int $height,int $weight){
+                $this->name =$name;
+                $this->height =$height;
+                $this->weight =$weight;
+            }
+            
+                
+        }
+
+        $Animal = new Animal('dog',60,5000);
+
+        print_r($Food);
+        echo '<br>';
+        print_r($Animal);
+        echo '<br>';
+        $Food->show_price();
+        $Animal->show_height();
+        ?>
+    </p>
+</body>
+
+</html>
